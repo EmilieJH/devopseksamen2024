334c6cfcc7d6fdb1d5d580c8e786de438a2a2b9d
Emilie <emiliehaldorsen@hotmail.com>
2024-11-14 23:36:53 +0100

    edited sqs.tf and event_source_mapping.tf

--- Oppgave2utkast2/infra/event_source_mapping.tf
+++ Oppgave2utkast2/infra/event_source_mapping.tf
@@ -3,5 +3,4 @@ resource "aws_lambda_event_source_mapping" "sqs_trigger" {
   function_name    = aws_lambda_function.sqs_lambda.arn
   batch_size       = 1
   enabled          = true
-  StartingPosition = "LATEST"
 }
--- Oppgave2utkast2/infra/sqs.tf
+++ Oppgave2utkast2/infra/sqs.tf
@@ -1,5 +1,5 @@
 resource "aws_sqs_queue" "image_queue" {
   name                       = var.sqs_queue_name
-  visibility_timeout_seconds = 60
+  visibility_timeout_seconds = 120
   message_retention_seconds  = 86400
 }
