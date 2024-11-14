5e1620bbfedadbff93c3c9ca9b4ac656c06d6ccd
Emilie <emiliehaldorsen@hotmail.com>
2024-11-14 23:38:06 +0100

    slettet første utkast

--- Oppgave2/infra/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "lambda_function_name" {
-  value = aws_lambda_function.image_generator_lambda.function_name
-}
-
-output "sqs_queue_url" {
-  value = aws_sqs_queue.image_generation_queue.url
-}
